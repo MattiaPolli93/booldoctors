b5e546b015e4416b64a11d5c0c5b24aece6f3f01
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-06-09 09:57:32 +0200

    add: all missing seeder

---
+++ database/seeds/SpecializationsTableSeeders.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SpecializationsTableSeeders extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
