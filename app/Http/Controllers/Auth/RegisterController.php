72707e25a0be7ab55ed2a732ecb27e2bacaabdd3
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-06-11 17:43:48 +0200

    enable: user registration & improve: RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'surname' => $data['surname'],
             'password' => Hash::make($data['password']),
         ]);
     }
