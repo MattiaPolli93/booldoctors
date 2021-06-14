09811a1c3d86ae985d1c8f90466cdce0cd094fdb
Mattia <matpolli93@gmail.com>
2021-06-14 14:24:35 +0200

    Create: header & footer files in partials folder

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,33 +1,27 @@
-{{-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Booldoctors</title>
-    </head>
-    <body> --}}
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Booldoctors</title>
+</head>
+<body>
 @extends('layouts.app')
-@section('content')
-    
-            <nav>
-                <div class="specializations">
-                    @foreach ($specializations as $specialization)
-                        <a href="#">{{$specialization['specialization']}}</a>
-                    @endforeach
-                     <p><i class="fas fa-clock"></i>Ciao</p>      
-                </div>
-            </nav>
+@section('content')    
+    <specs>
+        <div class="specializations">
+            @foreach ($specializations as $specialization)
+                <a href="#">{{$specialization['specialization']}}</a>
+            @endforeach
+        </div>
+    </specs>
 
-
-            <div>
-                @foreach ($sponsored_doctors as $doctor)
-                    <ul style="list-style: none">
-                        <li style="font-size: 26px">{{$doctor->name}} {{$doctor->surname}}</li>
-                    </ul>            
-                @endforeach        
-            </div> 
+    <div>
+        @foreach ($sponsored_doctors as $doctor)
+            <ul style="list-style: none">
+                <li style="font-size: 26px">{{$doctor->name}} {{$doctor->surname}}</li>
+            </ul>            
+        @endforeach        
+    </div> 
 @endsection
-    
-    {{-- </body>
-</html> --}}
