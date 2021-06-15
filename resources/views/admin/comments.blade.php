6f46f818f5df5d34bff465d54f0a4460c8c48932
Stefano <tepo@hotmail.it>
2021-06-15 17:54:21 +0200

    advance: comments & messages layout

--- resources/views/admin/comments.blade.php
+++ resources/views/admin/comments.blade.php
@@ -8,7 +8,14 @@
     <div class="container">
         <h1>I tuoi commenti</h1>
         @foreach ($comments as $comment)
+        <h6>Valutazione: {{$comment->rate}}</h6>
+        @if ($comment->comment != NULL)
         <p>{{$comment->comment}}</p>    
+        @else 
+        <p><em>Nessun commento</em></p>
+        @endif
+        <p>Inviato da: {{$comment->username}}</p>
+        <small>Data e orario del commento: {{$comment->added_on}}</small>
         @endforeach
         <p> <a href="{{ route('admin.profile.index') }}">Back to Homepage?</a> </p>
     </div>
--- resources/views/admin/messages.blade.php
+++ resources/views/admin/messages.blade.php
@@ -8,7 +8,9 @@
     <div class="container">
         <h1>I tuoi messaggi</h1>
         @foreach ($messages as $message)
-        <p>{{$message->message}}</p>    
+        <h5>Inviato da: {{$message->email}}</h5>
+        <p>{{$message->message}}</p>   
+        <small>Data e orario dell'invio: {{$message->added_on}}</small>
         @endforeach
         <p> <a href="{{ route('admin.profile.index') }}">Back to Homepage?</a> </p>
     </div>
