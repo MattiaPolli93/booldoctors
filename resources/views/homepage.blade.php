54c54306c5c54888d6fc4fbdb9ff83046fde84c4
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-06-16 22:56:15 +0200

    add: css hompage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,7 +4,39 @@
     Home
 @endsection
 
-@section('content')    
+@section('content')
+    <section class="my_jumbotron">
+        <div class="my_container clame">
+            <h1>Prenota la tua visita online!</h1>
+            <h3>Cerca tra 200 000 dottori.</h3>
+            <a><button class=".reset_btn my_action">Trova il tuo professionista</button></a>
+        </div>
+    </section>
+    <section class="infoShow">
+        <div class="my_container">
+            <div class="card">
+                <div>
+                    <i class="fas fa-thumbs-up"></i>
+                </div>
+                <h3>Il servizio di prenotazione è gratuito</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis tempora ad nobis eligendi odio aspernatur? Aspernatur deserunt expedita optio, qui ipsum, impedit laudantium eum ullam tempore, nulla sunt odit tenetur?</p>
+            </div>
+            <div class="card">
+                <div>
+                    <i class="fas fa-thumbs-up"></i>
+                </div>
+                <h3>Il servizio di prenotazione è gratuito</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis tempora ad nobis eligendi odio aspernatur? Aspernatur deserunt expedita optio, qui ipsum, impedit laudantium eum ullam tempore, nulla sunt odit tenetur?</p>
+            </div>
+            <div class="card">
+                <div>
+                    <i class="fas fa-thumbs-up"></i>
+                </div>
+                <h3>Il servizio di prenotazione è gratuito</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis tempora ad nobis eligendi odio aspernatur? Aspernatur deserunt expedita optio, qui ipsum, impedit laudantium eum ullam tempore, nulla sunt odit tenetur?</p>
+            </div>
+        </div>        
+    </section>    
     <specs>
         <div class="specializations">
             @foreach ($specializations as $specialization)
@@ -12,7 +44,6 @@
             @endforeach
         </div>
     </specs>
-
     <div>
         @foreach ($sponsored_doctors as $doctor)
             <ul style="list-style: none">
