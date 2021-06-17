e7ddb115c06f17c0aa47851bb81c391a3373b5cf
Stefano <tepo@hotmail.it>
2021-06-17 12:33:18 +0200

    advance: search page

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -8,11 +8,26 @@
 <div class="container" id="search">
     <div>
         <select name="spec" id="spec" v-model="spec" v-on:change="filterSpec">
+            <option value="" disabled selected>Filtra per specializzazione</option>
             @foreach ($specializations as $specialization)
                 <option :value="{{$specialization->id}}">{{$specialization->specialization}}</option>
             @endforeach
         </select>
     </div>
+    <div v-for="doctor in filterDoc" class="cardDoctor">
+        <div class="docAvatar">
+            <div class="image_box">
+                <img :src="'storage/'+ doctor.details.image" :alt="'Immagine di ' + doctor.name + ' ' + doctor.surname">
+            </div>
+        </div>
+        <div class="docInfo">
+            <h3>@{{doctor.name}} @{{doctor.surname}}</h3>
+            <span v-for="doc in doctor.specializations" class="my_tag">@{{doc.field}}</span>
+            <p>Indirizzo: @{{doctor.details.address}}</p>
+            <a :href="'http://127.0.0.1:8000/doctor/' + doctor.id">Contatta questo professionista</a>
+        </div>
+    </div>
+{{-- 
     <ul>
         <li v-for="doctor in filterDoc" class="mt-3">
             <h3>@{{doctor.name}} @{{doctor.surname}}</h3>
@@ -21,7 +36,7 @@
             <p v-for="doc in doctor.specializations">@{{doc.field}}</p>
 
         </li>
-    </ul>   
+    </ul>    --}}
 </div>
     <script src="https://unpkg.com/vue@next"></script>
     <script src="{{ asset('js/search.js') }}" defer></script>
