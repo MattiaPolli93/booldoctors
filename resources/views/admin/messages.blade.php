643dee3e2f51a9ffa3ab8442302d13e77547987f
Mattia <matpolli93@gmail.com>
2021-06-18 12:43:46 +0200

    Improve style: dashboard - messages

--- resources/views/admin/messages.blade.php
+++ resources/views/admin/messages.blade.php
@@ -5,14 +5,16 @@
 @endsection
 
 @section('content')
-    <div class="container">
-        <h1>I tuoi messaggi</h1>
-        @foreach ($messages as $message)
-        <h5>Inviato da: {{$message->email}}</h5>
-        <p>{{$message->message}}</p>   
-        <small>Data e orario dell'invio: {{$message->added_on}}</small>
-        @endforeach
-        <p> <a href="{{ route('admin.profile.index') }}">Back to Homepage?</a> </p>
+    <div class="my_container">
+        <div class="messages">
+            <h1><em>I tuoi messaggi</em></h1>
+            @foreach ($messages as $message)
+            <h5>Inviato da: {{$message->email}}</h5>
+            <p>{{$message->message}}</p>   
+            <small>Data e orario dell'invio: {{$message->added_on}}</small>
+            @endforeach
+            <p><a href="{{ route('admin.profile.index') }}">Torna alla Dashboard</a></p>
+        </div>
     </div>
 @endsection
 
