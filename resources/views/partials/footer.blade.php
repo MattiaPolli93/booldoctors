06206b8a1974a05135e329c84418d1e348ecdebc
Mattia <matpolli93@gmail.com>
2021-06-18 11:59:49 +0200

    Style: footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,12 +1,53 @@
 <footer>
-    <div class="my_container">
-        <h3>Footer</h3>
-        <ul>
-            <li><a href="#">Lorem</a></li>
-            <li><a href="#">Ipsum</a></li>
-            <li><a href="#">Dolor</a></li>
-            <li><a href="#">Sit</a></li>
-            <li><a href="#">Amet</a></li>
-        </ul>
+    <div class="my_container d-flex justify-content-between">
+        <div class="list-box">
+            <ul>
+                <li><a href="#">Servizi</a></li>
+                <li><a href="#">Privacy e cookies</a></li>
+                <li><a href="#">Chi siamo</a></li>
+                <li><a href="#">Contatti</a></li>
+                <li><a href="#">Lavoro</a></li>
+                <li><a href="#">Condizioni di Servizio</a></li>
+                <li><a href="#">Ufficio stampa</a></li>
+            </ul>
+        </div>
+        <div class="list-box">
+            <ul>
+                <li><a href="#">Per i pazienti</a></li>
+                <li><a href="#">Dottori</a></li>
+                <li><a href="#">Strutture</a></li>
+                <li><a href="#">Chiedi al dottore</a></li>
+                <li><a href="#">Prestazioni</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">App mobile</a></li>
+                <li><a href="#">Diagnostica per immagini</a></li>
+            </ul>
+        </div>
+        <div class="list-box">
+            <ul>
+                <li><a href="#">Per i professionisti</a></li>
+                <li><a href="#">Per i dottori</a></li>
+                <li><a href="#">Prezzi</a></li>
+                <li><a href="#">Risorse gratuite</a></li>
+                <li><a href="#">Pulsante di prenotazione</a></li>
+                <li><a href="#">Per i centri medici</a></li>
+                <li><a href="#">Centro Assistenza per Specialisti</a></li>
+                <li><a href="#">Consulenza online</a></li>
+            </ul>
+        </div>
+        <div class="list-box">
+            <ul>
+                <li><a href="#">Booldoctors</a></li>
+                <li><a href="#"><i class="fab fa-github"></i> Stefano Bodritti</a></li>
+                <li><a href="#"><i class="fab fa-github"></i> Gabriele Corti</a></li>
+                <li><a href="#"><i class="fab fa-github"></i> Giuseppe Lana</a></li>
+                <li><a href="#"><i class="fab fa-github"></i> Nicolò Matassoli</a></li>
+                <li><a href="#"><i class="fab fa-github"></i> Mattia Polli</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="lower-footer text-center">
+        <p><strong>BoolDoctors - Made with <span>&hearts;</span> by Team 6</strong></p>
     </div>
 </footer>
