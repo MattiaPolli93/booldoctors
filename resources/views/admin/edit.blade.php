6fbe6384a4f19b735b16d04aae0efc05d6e49bcb
Mattia <matpolli93@gmail.com>
2021-06-21 10:53:35 +0200

    Improve: edit - dashboard style

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -3,15 +3,23 @@
     Modifica profilo
 @endsection
 @section('content')
-  <div class="my_container">
+  <div class="my_container edit-form">
       <form action="{{route('admin.profile.update', $doctor->id)}}" method="POST" enctype="multipart/form-data">
           @csrf
           @method('PUT')
-          @if ($doctor->details->image != 'https://via.placeholder.com/150')
-          <img {{-- da modificare in caso di seed --}} src="{{ asset('storage/' . $doctor->details->image) }}" alt="Immagine di {{$doctor->name}} {{$doctor->surname}}">
-          @else
-          <img src="https://i.ibb.co/wQBsxBd/standard-Doctor.png" alt="Immagine del dottore">
-          @endif
+          <div class="info-box d-flex">
+              <div class="image-box">
+                  @if ($doctor->details->image != 'https://via.placeholder.com/150')
+                  <img {{-- da modificare in caso di seed --}} src="{{ asset('storage/' . $doctor->details->image) }}" alt="Immagine di {{$doctor->name}} {{$doctor->surname}}">
+                  @else
+                  <img src="https://i.ibb.co/wQBsxBd/standard-Doctor.png" alt="Immagine del dottore">
+                  @endif
+                </div>
+                <div class="text-box">
+                    <h4>Doctor <br> {{$doctor->name}} {{$doctor->surname}}</h4>
+                </div>
+
+          </div>
           <div class="form-group mt-3">
             <label for="image">Immagine</label>
             <input type="file" class="form-control" id='image' name='image'>
@@ -30,15 +38,7 @@
                 <textarea name="bio" id="bio" cols="60" rows="10">{{$doctor->details->bio}}</textarea>
             </div>
           </div>
-
-          <div class="form-group mt-4">
-            <h3 class="mb-3">Aggiungi nuova prestazione</h3>
-              <label for="service_name">Nome prestazione</label>
-              <input type="text" class="form-control mb-3" name="service_name" id="service_name" placeholder="Inserisci il nome della prestazione" value="">
-              <label for="service_price">Prezzo prestazione</label>
-              <input type="number" class="form-control" name="service_price" id="service_price" min="0" max="9999.99" step="0.01" placeholder="Inserisci il prezzo della prestazione" value="">
-          </div>
-
+          
           <div class="mt-4">
               <h3 class="mb-3">Specializzazioni</h3>
               @foreach ($specializations as $spec)
@@ -50,6 +50,14 @@
                 </div>
               @endforeach
             </div>
+
+          <div class="form-group mt-4">
+            <h3 class="mb-3">Aggiungi nuova prestazione</h3>
+              <label for="service_name">Nome prestazione</label>
+              <input type="text" class="form-control mb-3" name="service_name" id="service_name" placeholder="Inserisci il nome della prestazione" value="">
+              <label for="service_price">Prezzo prestazione</label>
+              <input type="number" class="form-control" name="service_price" id="service_price" min="0" max="9999.99" step="0.01" placeholder="Inserisci il prezzo della prestazione" value="">
+          </div>
 
           <button type="submit" class="btn btn-insert mt-3 mb-3">Inserisci</button>
         </form>
