67331cfdd92a81e9c5ce57979358e53c32e0fb97
Stefano <tepo@hotmail.it>
2021-06-22 10:43:41 +0200

    Improve: statistics page layout

--- resources/views/admin/statistics.blade.php
+++ resources/views/admin/statistics.blade.php
@@ -7,10 +7,12 @@
 @section('content')
 <div class="my_container">
   <h1 class="stat_title">Le tue statistiche</h1>
-  <div style="width: 85%; margin: auto; height: 500px;">
+  <div class="chart_1">
+    <h2>Numero di messaggi e recensioni ricevute ogni mese</h2>
     <canvas id="myChart"></canvas>
   </div>
-  <div style="width: 85%; margin: auto;">
+  <div class="chart_1">
+    <h2>Voti ricevuti ogni mese</h2>
     <canvas id="myOtherChart"></canvas>
   </div>
   <p class="link_dashboard"><a href="{{ route('admin.profile.index') }}">Torna alla Dashboard</a></p>
