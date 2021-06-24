5b0109d17ecf74949a5d46ec815965a789bf7952
Mattia <matpolli93@gmail.com>
2021-06-24 16:23:22 +0200

    Add: hover effects - show

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -26,7 +26,7 @@
     <div class="doctor_container">
         <h2 v-if="noFindTxt">Nessun dottore corrisponde a questa ricerca</h2>
         <div v-for="doctor in filterDoc" class="cardDoctor">
-            <div class="docAvatar">
+            <div class="docAvatar d-flex">
                 <div class="image_box">
                     <img v-if="doctor.details.image != null"{{-- da modificare in caso di seed --}} :src="'storage/' + doctor.details.image" :alt="'Immagine di ' + doctor.name + ' ' + doctor.surname">
                     <img v-else src="https://i.ibb.co/wQBsxBd/standard-Doctor.png" alt="Immagine del dottore">
