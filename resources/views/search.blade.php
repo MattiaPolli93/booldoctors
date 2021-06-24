e3410e11a58ea54af7278d99e3320f4894915c54
Mattia <matpolli93@gmail.com>
2021-06-24 18:07:51 +0200

    Improve: style search input fields - search page

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -8,21 +8,23 @@
 <div class="my_container" id="search">
     <div class="filter-options d-flex">
         <div class="filter_bar">
-            <select name="rate" id="rate" v-model="selectRate" v-on:change="prova">
-                <option selected disabled value="">Filtra per voto</option>
-                <option value="0" >0</option>
-                <option value="1" >1</option>
-                <option value="2" >2</option>
-                <option value="3" >3</option>
-                <option value="4" >4</option>
-                <option value="5" >5</option>
-            </select>
-            
-            <form name="myform" oninput="range1value.value = range1.valueAsNumber">
-                <label for="range1">Filtra per numero di recensioni</label>
+            <form name="myform" class="mb-3" oninput="range1value.value = range1.valueAsNumber">
+                <label for="range1">Filtra per numero di recensioni: </label> <br>
                 <input type="range" id="range" name="range1" min="0" :max="maxRange" step="1" value="numberOfRates" list="tickmarks" v-model="numberOfRates">
-                <output name="range1value" for="range1" >0</output>
+                <output name="range1value" for="range1">0</output>
             </form>
+
+            <div class="select-box">
+                <select name="rate" id="rate" v-model="selectRate" v-on:change="prova">
+                    <option selected disabled value="">Filtra per voto</option>
+                    <option value="0" >0</option>
+                    <option value="1" >1</option>
+                    <option value="2" >2</option>
+                    <option value="3" >3</option>
+                    <option value="4" >4</option>
+                    <option value="5" >5</option>
+                </select>
+            </div>
         </div>
         <div class="search_input">
             {{-- <select name="spec" id="spec" v-model="spec" v-on:change="filterSpec">
