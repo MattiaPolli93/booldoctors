d5caf521a11cc862d6145ad8cf8c21977a4a274c
Mattia <matpolli93@gmail.com>
2021-06-24 16:38:33 +0200

    Add: link to image - edit

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -43,10 +43,11 @@
     <div class="doctor_container">
         <h2 v-if="noFindTxt">Nessun dottore corrisponde a questa ricerca</h2>
         <div v-for="doctor in filterDoc" class="cardDoctor" v-show="doctor.RateInfo.averageRate >= selectRate && doctor.RateInfo.RateCout > numberOfRates">
-            <div class="docAvatar">
+            <div class="docAvatar d-flex">
                 <div class="image_box">
+                    <a :href="'http://127.0.0.1:8000/doctor/' + doctor.id">
                     <img v-if="doctor.details.image != null"{{-- da modificare in caso di seed --}} :src="'storage/' + doctor.details.image" :alt="'Immagine di ' + doctor.name + ' ' + doctor.surname">
-                    <img v-else src="https://i.ibb.co/wQBsxBd/standard-Doctor.png" alt="Immagine del dottore">
+                    <img v-else src="https://i.ibb.co/wQBsxBd/standard-Doctor.png" alt="Immagine del dottore"></a>
                 </div>
             </div>
             <div class="docInfo">
