2e35adc6542d47326addb0e6dafb41c96cdf2e74
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-06-26 16:16:52 +0200

    fix: navbar responsive

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,11 +4,11 @@
 
           <a class="navbar-brand" href="{{ route('homepage') }}"> <img src="{{url('/img/logotransp.png')}}" alt="BoolDoctors Logo">
           </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
-            </button>
+            </button> --}}
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="{{-- collapse --}} navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav mr-auto">
                     
