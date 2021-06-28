01a72128410ed9626be831019d45571d9f24afaa
Stefano <tepo@hotmail.it>
2021-06-28 11:03:06 +0200

    add: message after edit

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -210,5 +210,13 @@
         <hr>
     </section>
     {{-- fine sponsorships --}}    
+    @if (session('message'))
+    <div class="alert alert-success" style="position: fixed; bottom: 30px; right: 30px">
+        {{ session('message') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    @endif
 <script src="{{ asset('js/home.js') }}" defer></script>
 @endsection
